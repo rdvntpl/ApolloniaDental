eda7a2e45ddeb1a6207eeaa992935caa00c86333
rdvntpl <rdvntpl@gmail.com>
2025-01-07 22:58:17 +0300

    update

--- config/db.js
+++ config/db.js
@@ -1,12 +1,15 @@
 const mongoose = require('mongoose')
 
-const url = 'mongodb://localhost:27017/apolloniadentaldb'
+const url = process.env.MONGODB_URI || 'mongodb://ApolloDental:root@mongo:27017/apolloniadentaldb?authSource=admin'
 
 const connectDb = () => {
-    mongoose.connect(url ,).then( (res)=>{
+    mongoose.connect(url, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    }).then((res) => {
         console.log('Connected to MongoDB')
     }).catch(error => {
-        console.log(error)
+        console.log('MongoDB connection error:', error)
     })
 }
 
